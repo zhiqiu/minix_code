fb564bf22153090b57f47ed9c7f38c14f950fa79
zhiqiu <izhiqiu@foxmail.com>
2017-06-06 12:00:11 +0800

    update

--- driver/cs4624/cs4624.h
+++ driver/cs4624/cs4624.h
@@ -177,28 +177,28 @@ typedef struct DEV_STRUCT {
 	u32_t capt_ctl;
 } DEV_STRUCT;
 
-void dev_mixer_write(u32_t *base, u32_t reg, u32_t val);
-u32_t dev_mixer_read(u32_t *base, u32_t reg);
+void dev_mixer_write(DEV_STRUCT *dev, u32_t reg, u32_t val);
+u32_t dev_mixer_read(DEV_STRUCT *dev, u32_t reg);
 // u32_t == usigned long
 // u16_t == usigned int
 // u8_t  == usigned char
-void snd_mychip_pokeBA1(DEV_STRUCT * dev, u32_t reg, u32_t val){
+void snd_mychip_pokeBA1(DEV_STRUCT *dev, u32_t reg, u32_t val){
 	u16_t bank = reg >> 16;
 	u16_t offset = reg & 0xffff;
 	sdr_out32(dev->ba1_region.idx[bank].remap_addr, offset, val);
 }
 
-u32_t snd_mychip_peekBA1(DEV_STRUCT * dev, u32_t reg){
+u32_t snd_mychip_peekBA1(DEV_STRUCT *dev, u32_t reg){
 	u16_t bank = reg >> 16;
 	u16_t offset = reg & 0xffff;
 	return sdr_in32(dev->ba1_region.idx[bank].remap_addr, offset);
 }
 
-void snd_mychip_pokeBA0(DEV_STRUCT * dev, u32_t offset, u32_t val){
+void snd_mychip_pokeBA0(DEV_STRUCT *dev, u32_t offset, u32_t val){
 	sdr_out32(dev->ba0_region.idx[0].remap_addr, offset, val);
 }
 
-u32_t snd_mychip_peekBA0(DEV_STRUCT * dev, u32_t offset){
+u32_t snd_mychip_peekBA0(DEV_STRUCT *dev, u32_t offset){
 	return sdr_in32(dev->ba0_region.idx[0].remap_addr, offset);
 }
 
